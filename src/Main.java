9d86b7a2c7ad86b60b26708f4f46095bd5b6ae12
voddan <dgvodopyan@gmail.com>
2016-10-13 11:24:58 +0300

    Added dummy GUI and the start up procedures

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import gui.MainView;
+import javafx.application.Application;
+import kotlin.jvm.JvmClassMappingKt;
+import tornadofx.App;
+
 /**
  * Created by aglab2 on 07/10/16.
  */
-public class Main {
-    public static void main(String [] args)
-    {
-        Memory mem = new Memory();
-        mem.load_data((short) 0, (short) 123);
+public class Main extends App {
+    public Main() {
+        // some magic to initialise TornadoFX
+        super(JvmClassMappingKt.getKotlinClass(MainView.class));
+    }
+
+    public static void main(String[] args) {
+        Application.launch(Main.class);  // JavaFX start up procedure
     }
 }
---
+++ src/gui/MainView.kt
@@ -0,0 +1,15 @@
+package gui
+
+import javafx.scene.control.*
+import javafx.scene.layout.*
+import memory.*
+import tornadofx.*
+
+
+class MainView  : View() {
+    override val root = HBox(Label("Hello world"))
+
+    init {
+
+    }
+}
