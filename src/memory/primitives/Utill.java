0b9f138f8f3206046cf8de48c0685e7e756914ed
voddan <dgvodopyan@gmail.com>
2016-10-13 20:59:42 +0300

    fixed a bug

--- src/memory/primitives/Utill.java
+++ src/memory/primitives/Utill.java
@@ -3,7 +3,7 @@ package memory.primitives;
 
 public class Utill {
     static void assertIs2Bytes(int value) {
-        if(!(0 <= value && value < 0xFF))
+        if(!(0 <= value && value < 0xFFFF))
             throw new RuntimeException("value " + value + " is not ushort");
     }
 }
