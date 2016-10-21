ba9fd9398a2bbaedd1643db4594e6890a5546e35
Denis Kopyrin <denis.kopyrin@phystech.edu>
2016-10-21 15:23:27 +0300

    Added ImageGenerator -- basic init version.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import gui.MainView;
 import javafx.application.Application;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import kotlin.jvm.JvmClassMappingKt;
+import kotlin.reflect.KClass;
 import tornadofx.App;
+import videocard.ImageGenerator;
 
 /**
  *  NOTE: all start-up code is in the `MainController`.
@@ -12,11 +16,6 @@ public class Main extends App {
 
     public Main() {
         super(JvmClassMappingKt.getKotlinClass(MainView.class));
-
-//        Thread.setDefaultUncaughtExceptionHandler((thread, e) -> {
-//            Logger.getAnonymousLogger().log(Level.SEVERE, "JavaFX exception:", e);
-//            System.exit(1);
-//        });ParallelsTasks
     }
 
     public static void main(String[] args) {
---
+++ src/videocard/ImageGenerator.java
@@ -0,0 +1,25 @@
+package videocard;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by aglab2 on 21/10/16.
+ */
+
+/*
+  Image size is 256x256, 2 bits per pixel
+  This will take 256*256/4/1024=16 kB of memory
+*/
+
+public class ImageGenerator {
+    public BufferedImage image;
+
+    public ImageGenerator(int size){
+        image = new BufferedImage(size, size, BufferedImage.TYPE_BYTE_GRAY);
+    }
+
+    public void setPixel(int x, int y, int color){
+        image.setRGB(x, y, color);
+    }
+}
