3c0f69fc1a0805e0ea0fe8ce9dd8f5185030ac2d
aglab2 <denis.kopyrin@phystech.edu>
2017-01-20 21:38:02 +0300

    Added separate MAC files for keyboard and image

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 all:
 	make -C pdp11_sources
-	python make_rom.py
+	python make_rom.py default.rom source.pdp
+	python make_rom.py keyboard.rom kb.pdp
+	python make_rom.py picture.rom pic.pdp
--- make_rom.py
+++ make_rom.py
@@ -1,11 +1,17 @@
+import sys
+
+if len(sys.argv) < 3:
+	print ("Not enough arguments! Provide rom name and pdp source name")
+	exit(1)
+	
 def put_region(rom, name, offset):
 	with open(name, "rb") as source:
 		data = source.read()
 		rom.seek(offset)
 		rom.write(data)
 
-with open("./resources/default.rom", "wb") as out:
-	put_region(out, "./pdp11_sources/source.pdp", 0x0000)
+with open("./resources/" + sys.argv[1], "wb") as out:
+	put_region(out, "./pdp11_sources/" + sys.argv[2], 0x0000)
 	put_region(out, "./pdp11_scripts/dragon/dragon.zram", 0x1000)
 	put_region(out, "./pdp11_scripts/fonterino/fonterino_gray.font", 0x4000)
 	put_region(out, "./pdp11_sources/keyboardh.pdp", 0x7000)
